b19c81a15b2f45dacd552c0225c0f7c3ddbed21f
Mohammed-Ibrahim-15 <ibrahim72342@gmail.com>
2022-10-24 22:47:31 +0600

    enviroment variable added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCJy09VRbyP_LlvXXWNvSYL4hgMu5PtrVk",
-    authDomain: "cs-academy-client.firebaseapp.com",
-    projectId: "cs-academy-client",
-    storageBucket: "cs-academy-client.appspot.com",
-    messagingSenderId: "1041313351852",
-    appId: "1:1041313351852:web:e161098501a4f22a64b709"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
